d840605983b575168e99b3ed4a3d1ff816e65388
noam <noam.rozen@bondit.co.il>
2018-01-17 17:26:24 +0200

    Changed game manager such that the client also handles the game locally

--- game_manager.py
+++ game_manager.py
@@ -27,50 +27,62 @@ class GameServer(GameManager):
         super(GameServer, self).__init__(gui, communication_manager, game)
         # we might want a Player object, containing name, ip and other details about the player
         self.player = self.game.PLAYER_ONE
-
+        self.opponent = self.game.PLAYER_TWO
         # self.communication_manager.set_client_choice_handler(self.handle_client_choice)
 
-    def __handle_choice_wrapper(self, column, player):
-        if not self.game.get_current_player() == player:
-            raise errors.NotYourTurn()
+    def handle_choice(self, column):
+        if not self.game.get_current_player() == self.player:
+            raise ValueError(NOT_YOUR_TURN_ERROR_MSG)
 
-        cell_i, cell_j = self.game.get_column_cell(column)
-        if self.game.get_player_at(cell_i, cell_j) is None:
+        try:
             self.game.make_move(column)
-            if self.game.get_winner == self.player:
-                self.end_game(winner=self.player)
-        else:
-            raise errors.BlockedCell()
+            self.communication_manager.send_choice(column)
+            self.gui.output_board(self.game.board)
+            if self.game.get_winner() == self.player:
+                self.end_game(self.player)
+        except ValueError as e:
+            self.gui.output_error(str(e))
+        # cell_i, cell_j = self.game.get_column_cell(column)
+        # if self.game.get_player_at(cell_i, cell_j) is None:
+        #     self.game.make_move(column)
+        #     if self.game.get_winner == self.player:
+        #         self.end_game(winner=self.player)
+        # else:
+        #     raise errors.BlockedCell()
 
-        self.gui.output_board(self.game.board)
+
         # self.communication_manager.send_board_state(game)
 
-    def handle_choice(self, column):
-        try:
-            self.__handle_choice_wrapper(column, self.player)
-        except Exception as e:
-            self.gui.output_error(str(e))
+    # def handle_choice(self, column):
+    #     try:
+    #         self.__handle_choice_wrapper(column, self.player)
+    #     except Exception as e:
+    #         self.gui.output_error(str(e))
 
     def handle_client_choice(self, column):
-        try:
-            self.__handle_choice_wrapper(column, self.game.PLAYER_TWO)
-        except Exception as e:
-            self.communication_manager.send_error(str(e))
+        # try:
+        #     self.__handle_choice_wrapper(column, self.game.PLAYER_TWO)
+        # except Exception as e:
+        #     self.communication_manager.send_error(str(e))
+        self.game.make_move(column)
+        self.gui.output_board(self.game.board)
+        if self.game.get_winner() == self.opponent:
+            self.end_game(self.opponent)
 
 
-class GameClient(GameManager):
-    def __init__(self, gui, communicator, game):
-        super(GameClient, self).__init__(gui=gui, communication_manager=communicator, game=game)
-        self.communication_manager.set_server_answer_handler(self.handle_server_response)
-
-    def handle_choice(self, column):
-        self.communication_manager.send_choice(column)
-
-    def handle_server_response(self, message):
-        if isinstance(message, ErrorMessage):
-            self.gui.output_error(message.content)
-        if isinstance(message, UpdateMessage):
-            self.game.make_move(message.coordinates)
-            self.gui.output_board(self.game)
-        if isinstance(message, WinnerMessage):
-            self.end_game(message.content)
+# class GameClient(GameManager):
+#     def __init__(self, gui, communicator, game):
+#         super(GameClient, self).__init__(gui=gui, communication_manager=communicator, game=game)
+#         self.communication_manager.set_server_answer_handler(self.handle_server_response)
+#
+#     def handle_choice(self, column):
+#         self.communication_manager.send_choice(column)
+#
+#     def handle_server_response(self, message):
+#         if isinstance(message, ErrorMessage):
+#             self.gui.output_error(message.content)
+#         if isinstance(message, UpdateMessage):
+#             self.game.make_move(message.coordinates)
+#             self.gui.output_board(self.game)
+#         if isinstance(message, WinnerMessage):
+#             self.end_game(message.content)
